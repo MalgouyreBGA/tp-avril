fb3165cd8d52207a119475110037846a038814e4
bastien malgouyres <bmalgouyres@free.fr>
2023-04-21 16:37:15 +0200

    sort2

--- utils.js
+++ utils.js
@@ -1,3 +1,20 @@
+function getHeader(style){
+    let temp = ""
+    if(style === 'users'){
+        temp = '<div><a href="/">go Home</a></div>'
+    } else if (style == 'home'){
+        temp = '<div><a href="/users">go Users</a></div>'
+    }
+    return temp
+}
+form = '<form action="/users" method="POST">'
+    +'<div class="form-group">'
+    +'<label>Name<input class="form-control" name="name" type="text" /></label>'
+    +'<label>Birth<input class="form-control" name="birth" type="date" /></label>'
+    +'</div>'
+    +'<button type="submit" class="btn btn-primary">Ajouter</button>'
+    +'</form>'
+
 function lines(students){
     let htmlData = []
     students.forEach(student=>{
@@ -15,22 +32,5 @@ function lines(students){
 function button(index){
     return '<button type="submit" action="/users/'+index+'" method="DELETE">X</button>'
 }
-
-function getHeader(style){
-    let temp = ""
-    if(style === 'users'){
-        temp = '<div><a href="/">go Home</a></div>'
-    } else if (style == 'home'){
-        temp = '<div><a href="/users">go Users</a></div>'
-    }
-    return temp
-}
-form =  '<form action="/users" method="POST">'
-        +'<div class="form-group">'
-        +'<label>Name<input class="form-control" name="name" type="text" /></label>'
-        +'<label>Birth<input class="form-control" name="birth" type="date" /></label>'
-        +'</div>'
-        +'<button type="submit" class="btn btn-primary">Ajouter</button>'
-        +'</form>'
 
 module.exports = { lines, getHeader, form }
